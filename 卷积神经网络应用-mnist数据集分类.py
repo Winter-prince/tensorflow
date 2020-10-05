24cdc5d7acd3858549bcefd3b757615bbc22d6cd
Winter-prince <sunraiser3510@gmail.com>
2020-10-05 17:41:28 +0800

    formmat
    formmat

--- 卷积神经网络应用-mnist数据集分类.py
+++ 卷积神经网络应用-mnist数据集分类.py
@@ -14,7 +14,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
 batch_size = 100  # 一个批次的大小，可优化
-n_batch = mnist.train.num_examples // batch_size # 一共有多少个批次
+n_batch = mnist.train.num_examples // batch_size  # 一共有多少个批次
 
 
 # 权值初始化
@@ -61,28 +61,28 @@ b_con2 = bias_variable([64])  # 每一次卷积对应一个偏置，所以32
 h_con2 = tf.nn.relu(conv2d(h_pool1, w_con2) + b_con2)
 h_pool2 = max_pool_2x2(h_con2)
 
-#28*28*1的图片第一次卷积后tensor的shape是28*28*32，第一次池化后是14*14*32
-#第二次卷积后输出shape是14*14*64，第二次池化后是7*7*64
+# 28*28*1的图片第一次卷积后tensor的shape是28*28*32，第一次池化后是14*14*32
+# 第二次卷积后输出shape是14*14*64，第二次池化后是7*7*64
 
-#第一个全连接层的权值和偏置
-w_fc1=weight_variable([7*7*64,1024])
-b_fc1=bias_variable([1024])
-#池化后的输出扁平化为1维
-h_pool2_flat=tf.reshape(h_pool2,[-1,7*7*64])
-#第一个全连接层的输出
-h_fc1=tf.nn.relu(tf.matmul(h_pool2_flat,w_fc1)+b_fc1)
+# 第一个全连接层的权值和偏置
+w_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+# 池化后的输出扁平化为1维
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+# 第一个全连接层的输出
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
 
-#神经元输出概率，防止过拟合
-keep_prob=tf.placeholder(tf.float32)
-h_fc1_drop=tf.nn.dropout(h_fc1,keep_prob)
+# 神经元输出概率，防止过拟合
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-#第二个全连接层
-w_fc2=weight_variable([1024,10])
-b_fc2=bias_variable([10])
+# 第二个全连接层
+w_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
 predict = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)  # softmax将输出信号转化为概率值（10个概率值）
 
 # 交叉熵代价函数
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y,logits=predict))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=predict))
 # 使用AdamOptimizer优化，使得loss最小
 train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 # 初始化变量
@@ -98,7 +98,6 @@ with tf.Session() as sess:
     for epoch in range(21):  # 可优化
         for batch in range(n_batch):  # 把所有图片训练一次
             batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-            sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys,keep_prob:0.7})
+            sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 0.7})
         # 用测试数据来检验训练好的模型
-        acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels,keep_prob:1.0})
         print("Iter " + str(epoch) + "Test accuracy" + str(acc))
